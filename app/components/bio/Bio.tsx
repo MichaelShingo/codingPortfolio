d31c32455c377ec96fd9dd57f6c34e35ae2c80e2
MichaelShingo <mcrawford5376@gmail.com>
2024-02-20 19:28:33 +0100

    bio arrow complete

--- app/components/bio/Bio.tsx
+++ app/components/bio/Bio.tsx
@@ -1,23 +1,37 @@
+'use client';
+
 const Bio: React.FC = () => {
 	return (
-		<div className="flex h-screen w-screen translate-y-[15%] justify-center">
-			<div className="flex h-fit w-[90%] flex-col space-y-3 border-[3px] border-solid border-black p-5 text-justify text-sm font-light sm:w-[80%] md:w-[50%] md:text-lg lg:w-[50%]">
-				<p className="">
-					I am a software developer and musician with a passion for exploring the
-					intersections between tech and the arts, helping creative professionals advance
-					their skills and careers. As a web developer proficient in React, Typescript,
-					and Python, I craft seamless user experiences through robust and reliable
-					functionality alongside intuitive, responsive, and beautiful UI. <br></br>
-				</p>
-				<p className="">
-					My expertise extends to backend and API development, granting me a holistic
-					understanding of the applications to which I contribute. Beyond my numerous
-					personal projects, I have collaborated with diverse teams and clients on both
-					freelance and volunteer work, consistently delivering exceptional user
-					experiences. With every application that I build, I strive to write clean,
-					maintainable code while drawing from a rich palette of knowledge acquired
-					through my dual roles as a developer and creative professional.
-				</p>
+		<div className="flex h-screen w-screen justify-center">
+			<div className="flex h-fit w-[90%] translate-y-[35%] flex-col items-center justify-center space-y-3 border-[3px] border-solid border-black bg-paper-white p-5 text-justify text-sm font-light sm:w-[80%] md:w-[55%] md:text-lg lg:text-2xl">
+				<div className="">
+					<p className="">
+						I am a software developer and musician with a passion for exploring the
+						intersections between tech and the arts, helping creative professionals
+						advance their skills and careers. As a web developer proficient in React,
+						Typescript, and Python, I craft seamless user experiences through robust and
+						reliable functionality alongside intuitive, responsive, and beautiful UI.{' '}
+						<br></br>
+						<br></br>
+					</p>
+					<p className="">
+						My expertise extends to backend and API development, granting me a holistic
+						understanding of the applications to which I contribute. Beyond my numerous
+						personal projects, I have collaborated with diverse teams and clients on both
+						freelance and volunteer work, consistently delivering exceptional user
+						experiences. With every application that I build, I strive to write clean,
+						maintainable code while drawing from a rich palette of knowledge acquired
+						through my dual roles as a developer and creative professional.
+					</p>
+				</div>
+				<div></div>
+				<button className="absolute bottom-0 h-[30%] w-[3px] translate-y-[100%] bg-black transition-all duration-700 hover:h-[35%]">
+					<div className="absolute h-[120%] w-5 translate-x-[-50%] translate-y-[-50%] bg-transparent"></div>
+					<img
+						src="/arrowHead.svg"
+						className="absolute bottom-0 h-4 translate-y-[70%] rotate-90 scale-[5] bg-center"
+					/>
+				</button>
 			</div>
 		</div>
 	);
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -13,11 +13,10 @@ const NavbarItem: React.FC<NavbarItemProps> = ({ title }) => {
 	const windowWidth: number = useAppSelector(
 		(state) => state.windowReducer.value.windowWidth
 	);
-	const dispatch = useDispatch<AppDispatch>();
 	const ref = useRef<HTMLButtonElement>(null);
+	const dispatch = useDispatch<AppDispatch>();
 
 	useEffect(() => {
-		// on window width and height change, calculate new boundingBox
 		if (currentPage === title && ref.current) {
 			const rect: DOMRect = ref.current?.getBoundingClientRect();
 			const newBoundingBox: BoundingBox = {
@@ -28,7 +27,7 @@ const NavbarItem: React.FC<NavbarItemProps> = ({ title }) => {
 			};
 			dispatch(setBoundingBox(newBoundingBox));
 		}
-	}, [currentPage, windowWidth]);
+	}, [currentPage, windowWidth, title, dispatch]);
 	const handleOnClick = () => {
 		dispatch(setPage(title));
 	};
@@ -36,7 +35,7 @@ const NavbarItem: React.FC<NavbarItemProps> = ({ title }) => {
 		<button
 			ref={ref}
 			onClick={handleOnClick}
-			className="text-center text-xl font-light transition sm:text-3xl md:text-5xl"
+			className="p-1 text-center text-xl font-light transition duration-300 hover:scale-[95%] sm:text-3xl md:text-5xl"
 		>
 			{title}
 		</button>
@@ -49,7 +48,7 @@ const Navbar: React.FC = () => {
 	return (
 		<div
 			ref={ref}
-			className="absolute mt-4 flex h-fit w-screen flex-row items-center justify-center space-x-9 sm:space-x-11 md:space-x-20"
+			className="fixed mt-4 flex h-fit w-screen flex-row items-center justify-center space-x-9 sm:space-x-11 md:space-x-20"
 		>
 			<NavbarItem title="Bio" />
 			<NavbarItem title="Portfolio" />
