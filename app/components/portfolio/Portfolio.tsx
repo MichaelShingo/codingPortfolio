7cd441aa5a0a3e61df9c5ff1a8eb91efaee5ac91
MichaelShingo <mcrawford5376@gmail.com>
2024-02-22 13:12:47 +0100

    portfolio preview hover

--- app/components/portfolio/Portfolio.tsx
+++ app/components/portfolio/Portfolio.tsx
@@ -3,65 +3,7 @@ import { motion, useTransform, useScroll } from 'framer-motion';
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import ProjectToggleButton from './ProjectToggleButton';
 import { useAppSelector } from '@/redux/store';
-
-const sampleJSON = [
-	{
-		title: 'Miitronome',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: true,
-	},
-	{
-		title: 'Violintice',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: true,
-	},
-	{
-		title: 'For the Lost Creative',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: true,
-	},
-	{
-		title: 'AnnaLyze',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: true,
-	},
-	{
-		title: 'Miitronome',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: false,
-	},
-	{
-		title: 'Violintice',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: false,
-	},
-	{
-		title: 'For the Lost Creative',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: false,
-	},
-	{
-		title: 'AnnaLyze',
-		preview: 'Miitronome is a metronome app build with Typescript.',
-		tags: ['Next.js', 'Typescript', 'MUI'],
-		logo: '/miitronomeLogoPortfolio.svg',
-		showcase: false,
-	},
-];
+import { sampleJSON } from '@/app/utils/sampleData';
 
 type PortfolioItem = {
 	title: string;
@@ -74,24 +16,21 @@ type PortfolioItem = {
 const Portfolio = () => {
 	const [isShowcase, setIsShowcase] = useState<boolean>(true);
 	const [portfolioData, setPortfolioData] = useState<PortfolioItem[]>([]);
-	const [filteredPortfolioData, setFilteredPortfolioData] = useState<PortfolioItem[]>([]);
-
-	const verticalScrollRef = useRef<HTMLDivElement | null>(null);
-	const horizontalScrollRef = useRef<HTMLDivElement | null>(null);
-
+	const [filteredPortfolioData, setFilteredPortfolioData] = useState<PortfolioItem[]>(
+		portfolioData.filter((item) => item.showcase)
+	);
 	const windowWidth: number = useAppSelector(
 		(state) => state.windowReducer.value.windowWidth
 	);
+	const verticalScrollRef = useRef<HTMLDivElement | null>(null);
+
 	const { scrollYProgress } = useScroll({ target: verticalScrollRef });
 	const xScroll = useTransform(scrollYProgress, [0, 1], ['0%', '-230%']);
 	const buttonOpacity = useTransform(scrollYProgress, [0, 1], [1, 0]);
 
-	console.log(scrollYProgress);
-
 	useEffect(() => {
 		setTimeout(() => {
 			setPortfolioData(sampleJSON);
-			setFilteredPortfolioData(sampleJSON);
 		}, 1000);
 	}, []);
 
@@ -101,7 +40,7 @@ const Portfolio = () => {
 		} else {
 			setFilteredPortfolioData(portfolioData);
 		}
-	}, [isShowcase]);
+	}, [isShowcase, portfolioData]);
 
 	const handleOnClick = () => {
 		setIsShowcase((prev) => !prev);
@@ -111,28 +50,44 @@ const Portfolio = () => {
 
 	const generatePortfolioItems = (containerWidth: number): ReactNode[] => {
 		const res: ReactNode[] = [];
-		if (!horizontalScrollRef.current) {
-			return res;
-		}
-
 		const containerIncrements: number = containerWidth / filteredPortfolioData.length;
 
 		for (let i = 0; i < filteredPortfolioData.length; i++) {
+			const currentItem = filteredPortfolioData[i];
 			const positionVariance: number = (Math.random() - 0.5) * 50;
-			const offset: number = positionVariance + 300;
+			const offset: number = positionVariance + 400;
 			const horizontalPosition: number = Math.round(i * containerIncrements) + offset;
 			const isAbove: number = i % 2 === 0 ? -1 : 1;
 			const verticalPosition = isAbove * 250 + positionVariance;
 
+			const generateTags = (): ReactNode[] => {
+				const res: ReactNode[] = [];
+				for (const tag of currentItem.tags) {
+					res.push(
+						<p className="m-1 w-fit rounded-sm bg-black px-1 py-[1px] text-paper-white">
+							{tag}
+						</p>
+					);
+				}
+				return res;
+			};
+
 			res.push(
 				<button
-					className={`absolute z-10 aspect-square w-[65px] rounded-full border-[2px] border-black bg-paper-white transition duration-700`}
+					className={`group absolute z-10 flex aspect-square w-[65px] flex-row items-center justify-center rounded-full border-[2px] border-black bg-paper-white transition duration-700`}
 					style={{
 						left: `${horizontalPosition}px`,
 						transform: `translateY(${verticalPosition}%) scale(100%)`,
 					}}
 				>
-					<img src={filteredPortfolioData[i].logo}></img>
+					<div className="absolute -z-10 h-[2px] w-[65%] translate-x-[0%] translate-y-[100%] bg-black opacity-0 transition duration-700 group-hover:translate-x-[100%] group-hover:opacity-100"></div>
+					<img className="" src={filteredPortfolioData[i].logo}></img>
+					<div className="pointer-events-none absolute max-h-[150px] min-w-[200px] translate-x-[50%] border-[2px] border-black bg-paper-white p-2 opacity-0 transition duration-700 group-hover:translate-x-[80%] group-hover:opacity-100">
+						<p className="font-thin">
+							{currentItem.title} - {currentItem.preview}
+						</p>
+						<div className="flex flex-row justify-center">{generateTags()}</div>
+					</div>
 				</button>
 			);
 		}
@@ -158,11 +113,8 @@ const Portfolio = () => {
 					style={{ x: xScroll }}
 					className="overflow-show sticky top-[26vh] w-screen items-center justify-center"
 				>
-					<div
-						ref={horizontalScrollRef}
-						className="flex h-[70vh] w-[4vw] items-center justify-center"
-					>
-						{portfolioData && generatePortfolioItems(windowWidth * 2)}
+					<div className="flex h-[70vh] w-[4vw] items-center justify-center">
+						{filteredPortfolioData && generatePortfolioItems(windowWidth * 2)}
 						<img
 							src="/chaconne.svg"
 							className="pointer-events-none absolute left-[55%] scale-[200%]"
---
+++ app/utils/sampleData.tsx
@@ -0,0 +1,58 @@
+export const sampleJSON = [
+	{
+		title: 'Miitronome',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: true,
+	},
+	{
+		title: 'Violintice',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: true,
+	},
+	{
+		title: 'For the Lost Creative',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: true,
+	},
+	{
+		title: 'AnnaLyze',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: true,
+	},
+	{
+		title: 'Miitronome',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: false,
+	},
+	{
+		title: 'Violintice',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: false,
+	},
+	{
+		title: 'For the Lost Creative',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: false,
+	},
+	{
+		title: 'AnnaLyze',
+		preview: 'Miitronome is a metronome app build with Typescript.',
+		tags: ['Next.js', 'Typescript', 'MUI'],
+		logo: '/miitronomeLogoPortfolio.svg',
+		showcase: false,
+	},
+];
